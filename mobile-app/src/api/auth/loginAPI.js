f2acd207fb34bc87d2b6f7fc8c818fa1bcbc1a87
Aidan Andrew-Hodgert <hodgerta@gmail.com>
2023-07-03 14:15:16 -0500

    api library

---
+++ mobile-app/src/api/auth/loginAPI.js
@@ -0,0 +1,85 @@
+import axios from 'axios';
+require('dotenv').config();
+const baseURL = process.env.REACT_APP_BASE_URL;
+const API = baseURL + '/authentication/';
+
+class LoginAPI{
+
+    static async loginApple(token){
+        const url = API + 'loginApple';
+
+        const body = {
+            token: token
+        }
+
+        try{
+            await axios.post(url, body);
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+
+    static async loginGoogle(token){
+        const url = API + 'loginGoogle';
+
+        const body = {
+            token: token
+        }
+
+        try{
+            await axios.post(url, body);
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+
+    static async loginOTP(email, otp){
+        const url = API + 'loginOTP';
+
+        const body = {
+            email: email,
+            otp: otp
+        }
+
+        try{
+            await axios.post(url, body);
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+
+    static async requestOTP(email){
+        const url = API + 'requestOTP';
+
+        const body = {
+            email: email
+        }
+
+        try{
+            await axios.post(url, body);
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+
+    static async refreshToken(token){
+        const headers = {
+            'Authorization': `Bearer ${token}`
+        };
+        const url = API + 'refreshToken';
+        const body = {};
+
+        try{
+            await axios.post(url, body, {headers: headers});
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+
+}
+export default LoginAPI;
