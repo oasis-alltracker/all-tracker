1addb8cd4c40993abb7a79cfad6708d668616428
Aidan Andrew-Hodgert <hodgerta@gmail.com>
2023-12-01 19:56:33 -0700

    remove original frontend

--- mobile-app/src/api/DB/exercisesDB.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-class ExercisesDB {
-  static async searchExercises() {
-    return ["pull-ups", "push-ups"];
-  }
-}
-export default ExercisesDB;
--- mobile-app/src/utils/commonUtils.js
+++
@@ -1,4 +0,0 @@
-export const isEmailValid = (email) => {
-    const emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return emailReg.test(email)
-}
