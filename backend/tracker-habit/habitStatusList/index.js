b36e4465af79582b1129d839058fe9b1cdb83e74
Aidan Andrew-Hodgert <hodgerta@gmail.com>
2024-02-05 20:49:40 -0700

    moved stausList logic from front end to backend

---
+++ backend/tracker-habit/habitStatusList/index.js
@@ -0,0 +1,36 @@
+const DynamoDB = require("aws-sdk/clients/dynamodb");
+const DbUtils = require("../../utils/databaseManager");
+
+const tableName = process.env.ALL_TRACKER_TABLE_NAME;
+const DB = new DynamoDB.DocumentClient();
+const dbService = new DbUtils(DB, tableName);
+
+const GetHabitStatusList = require("./getHabitStatusList");
+const getHabitStatusList = new GetHabitStatusList(dbService);
+
+const { authenticateToken } = require("../../utils/authenticateToken");
+
+module.exports.handler = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const user = authenticateToken(event.headers);
+  var response;
+
+  if (!user?.email) {
+    callback(null, {
+      statusCode: 401,
+      body: JSON.stringify("Unauthorized"),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+    });
+  }
+
+  if (event.httpMethod == "GET") {
+    response = await getHabitStatusList.getHabitStatusList(
+      user,
+      JSON.parse(event.body)
+    );
+  }
+  callback(null, response);
+};
