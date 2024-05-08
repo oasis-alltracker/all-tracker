6a397ef4e378ccdedea6f0a67838e44357d32576
Aidan Andrew-Hodgert <hodgerta@gmail.com>
2024-05-07 21:10:09 -0600

    delete task modal and aggregate to one

--- mobile-app/src/screens/Todos-Habits/modals/TaskModal.js
+++ mobile-app/src/screens/Todos-Habits/modals/TaskModal.js
@@ -10,28 +10,135 @@ import RNModal from "react-native-modal";
 import { Image } from "react-native";
 import { Button, Calendar } from "../../../components";
 
-export default function CreateTaskModal({ getRef }) {
+export default function TaskModal({
+  getRef,
+  createTask,
+  createToDo,
+  updateTask,
+  updateToDo,
+  deleteTask,
+  deleteToDo,
+}) {
   const [visible, setVisible] = useState(false);
-  const [isCheck, setIsCheck] = useState(false);
+
+  const [isEdit, setIsEdit] = useState(false);
 
   const [title, setTitle] = useState("");
-  const [desc, setDesc] = useState("");
-  const [edit, setEdit] = useState(false);
+  const [description, setDescription] = useState("");
+  const [itemID, setItemID] = useState(null);
+  const [isRecurring, setIsRecurring] = useState(false);
+  const [time, setTime] = useState(null);
+  const [dateStamp, setDateStamp] = useState(null);
+  const [schedule, setSchedule] = useState(null);
 
   const calendarRef = useRef(null);
+
+  const saveDateHandler = async (
+    newIsRecurring,
+    newTime,
+    newDateStamp,
+    newSchedule
+  ) => {
+    setIsRecurring(newIsRecurring);
+    setTime(newTime);
+    setDateStamp(newDateStamp);
+    setSchedule(newSchedule);
+  };
+
+  const onBack = async () => {
+    setVisible(false);
+    if (isEdit) {
+      if (isRecurring) {
+        await deleteTask(itemID);
+      } else {
+        await deleteToDo(itemID);
+      }
+    }
+  };
+
+  const onSave = async () => {
+    setVisible(false);
+    if (!taskName && !description) {
+      Toast.show("You must complete the form to create a habit.", {
+        ...styles.errorToast,
+        duration: Toast.durations.LONG,
+      });
+    } else if (!taskName) {
+      Toast.show("Don't forget to give this task a name.", {
+        ...styles.errorToast,
+        duration: Toast.durations.LONG,
+      });
+    } else if (!description) {
+      Toast.show("Don't forget to give this task a description.", {
+        ...styles.errorToast,
+        duration: Toast.durations.LONG,
+      });
+    } else {
+      try {
+        if (isEdit) {
+          if (isRecurring) {
+            task = {
+              name: title,
+              schedule: schedule,
+              description: description,
+              dateStamp: dateStamp,
+            };
+            await updateTask(task, time, schedule);
+          } else {
+            toDo = {
+              name: title,
+              description: description,
+              dateStamp: dateStamp,
+            };
+            await updateToDo(toDo, time, dateStamp);
+          }
+        } else {
+          if (isRecurring) {
+            task = {
+              name: title,
+              schedule: schedule,
+              description: description,
+              dateStamp: dateStamp,
+            };
+            await createTask(task, time, schedule);
+          } else {
+            toDo = {
+              name: title,
+              description: description,
+              dateStamp: dateStamp,
+            };
+            await createToDo(toDo, time, dateStamp);
+          }
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
 
   useEffect(() => {
     let ref = {
       open(isEdit = false, props) {
         setVisible(true);
         if (isEdit) {
+          setIsEdit(true);
+
           setTitle(props.title);
-          setDesc(props.desc);
-          setEdit(true);
+          setDescription(props.description);
+          setIsRecurring(props.isRecurring);
+          setTime(props.time);
+          setDateStamp(props.dateStamp);
+          setSchedule(props.schedule);
+          setItemID(props.itemID);
         } else {
           setTitle("");
-          setDesc("");
-          setEdit(false);
+          setDescription("");
+          setIsRecurring(false);
+          setTime(null);
+          setDateStamp("noDueDate");
+          setSchedule(null);
+          setItemID(null);
+          setIsEdit(false);
         }
       },
       close() {
@@ -60,7 +167,7 @@ export default function CreateTaskModal({ getRef }) {
           />
           <TouchableOpacity
             onPress={() => {
-              calendarRef.current.open();
+              calendarRef.current.open(isEdit);
             }}
           >
             <Image
@@ -79,23 +186,23 @@ export default function CreateTaskModal({ getRef }) {
             placeholderTextColor={"#7B97BC"}
             placeholder="Description"
             style={styles.input}
-            onChangeText={setDesc}
-            value={desc}
+            onChangeText={setDescription}
+            value={description}
           />
         </View>
         <View style={styles.buttonsRow}>
-          <Button
-            onPress={() => setVisible(false)}
-            style={[styles.button, styles.back]}
-          >
-            {edit ? "Delete" : "Cancel"}
+          <Button onPress={() => onBack()} style={[styles.button, styles.back]}>
+            {isEdit ? "Delete" : "Cancel"}
           </Button>
-          <Button onPress={() => setVisible(false)} style={styles.button}>
-            Ok
+          <Button onPress={() => onSave()} style={styles.button}>
+            Save
           </Button>
         </View>
       </View>
-      <Calendar getRef={(ref) => (calendarRef.current = ref)} />
+      <Calendar
+        saveDateHandler={saveDateHandler}
+        getRef={(ref) => (calendarRef.current = ref)}
+      />
     </RNModal>
   );
 }
