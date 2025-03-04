13d9cd463cd9e9882adfdb9743d873f917c863f9
Aidan Andrew-Hodgert <hodgerta@gmail.com>
2025-03-03 20:59:57 -0700

    Add missing Alert import statement

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -8,7 +8,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import { store, persistor } from "./src/store";
 import { PersistGate } from "redux-persist/integration/react";
-import { ActivityIndicator, StyleSheet, Platform } from "react-native";
+import { ActivityIndicator, StyleSheet, Platform, Alert } from "react-native";
 import { getAccessToken, isLoggedIn, logout } from "./src/user/keychain";
 import UserAPI from "./src/api/user/userAPI";
 import * as Notifications from "expo-notifications";
