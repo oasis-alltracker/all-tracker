b1c6ca7fbeb61d9e5687fd83104264a9592b9a5d
BasmaAbdulRazaq <123525422+BasmaAbdulRazaq@users.noreply.github.com>
2025-05-22 09:12:36 -0500

    meal macros calculation backend (#79)
    Co-authored-by: basmaabdulrazaq <abdulrab@myumanitoba.ca>

---
+++ backend/tracker-diet/foodEntriesMacros/index.js
@@ -0,0 +1,41 @@
+const { DynamoDBDocument } = require("@aws-sdk/lib-dynamodb");
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+
+const DbUtils = require("../../utils/databaseManager");
+
+const tableName = process.env.ALL_TRACKER_TABLE_NAME;
+const DB = DynamoDBDocument.from(new DynamoDB());
+const dbService = new DbUtils(DB, tableName);
+
+const GetMealMacros = require("./getMealMacros");
+const getMealMacros = new GetMealMacros(dbService);
+
+
+const { authenticateToken } = require("../../utils/authenticateToken");
+
+module.exports.handler = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const user = authenticateToken(event.headers);
+
+  var response;
+
+  if (!user?.email) {
+    callback(null, {
+      statusCode: 401,
+      body: JSON.stringify("Unauthorized"),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+    });
+  }
+
+  if (event.httpMethod == "GET") {
+    response = await getMealMacros.getMealMacros(
+      user,
+      event.queryStringParameters,
+    );
+  } 
+
+  callback(null, response);
+};
