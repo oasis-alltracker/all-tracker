e25fdcf0a4776918b2c5abd8c9a0df3f8f3162a5
KarinP03 <k.plavin03@gmail.com>
2025-06-05 14:23:53 -0500

    Made modal look like goals page

--- mobile-app/src/screens/Fitness-Diet/modals/EditMacroGoalsModal.js
+++ mobile-app/src/screens/Fitness-Diet/modals/EditMacroGoalsModal.js
@@ -1,16 +1,120 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import RNModal from "react-native-modal";
 import navigationService from "../../../navigators/navigationService";
+import { Header } from "../../../components";
+import DietGoalsAPI from "../../../api/diet/dietGoalsAPI";
+import { getAccessToken } from "../../../user/keychain";
+import Spinner from "react-native-loading-spinner-overlay";
+import UpdateMacrosModal from "../../Setup/Diet/UpdateMacrosModal";
 
 
 export default function EditMacroGoalsModal({ isVisible, setVisible }) {
-  const [selectedIndex, setSelected] = useState(-1);
+  const [datas, setDatas] = useState([
+      {
+        title: "0 g",
+        img: require("../../../assets/images/carbs.png"),
+        text: "Carbs:",
+      },
+      {
+        title: "0 g",
+        img: require("../../../assets/images/protein.png"),
+        text: "Protein:",
+      },
+      {
+        title: "0 g",
+        img: require("../../../assets/images/fats.png"),
+        text: "Fats:",
+      },
+    ]);
+    const [calorieGoalValue, setCalorieGoalValue] = useState(0);
+    const [calories, setCalories] = useState(0);
+    const [calorieUnit, setCalorieUnit] = useState("kcal");
+    const [carbGoalValue, setCarbGoalValue] = useState(0);
+    const [proteinGoalValue, setProteinGoalValue] = useState(0);
+    const [fatGoalValue, setFatGoalValue] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+  
+    const updateMacrosRef = useRef(null);
 
-  const updateSelected = (currIndex) => {
-    if (selectedIndex == currIndex) setSelected(-1);
-    else setSelected(currIndex);
+const onUpdateMacroValue = async (title, value, units) => {
+    var newCalories = calorieGoalValue;
+    var newFats = fatGoalValue;
+    var newProteins = proteinGoalValue;
+    var newCarbs = carbGoalValue;
+    if (title === "Calories") {
+      setCalories(value);
+      setCalorieUnit(units);
+      newCalories = { value: value, units: units };
+      setCalorieGoalValue(newCalories);
+    } else {
+      let newDatas = datas.map((item) => {
+        if (item.text === title) {
+          if (title === "Carbs:") {
+            newCarbs = value;
+            setCarbGoalValue(value);
+          }
+          if (title === "Protein:") {
+            newProteins = value;
+            setProteinGoalValue(value);
+          }
+          if (title === "Fats:") {
+            newFats = value;
+            setFatGoalValue(value);
+          }
+          return {
+            ...item,
+            title: value + " " + units,
+          };
+        }
+        return item;
+      });
+      setDatas(newDatas);
+      const token = await getAccessToken();
+      DietGoalsAPI.updateDietGoals(token, {
+        carbGoal: newCarbs,
+        proteinGoal: newProteins,
+        fatGoal: newFats,
+        calorieGoal: newCalories,
+      });
+    }
   };
+
+  useEffect(() => {
+    const getDataOnLoad = async () => {
+      setIsLoading(true);
+      const token = await getAccessToken();
+      const dietGoals = await DietGoalsAPI.getDietGoals(token);
+
+      setCalorieGoalValue(dietGoals.calorieGoal);
+      setCalorieUnit(dietGoals.calorieGoal.units);
+      setCalories(dietGoals.calorieGoal.value);
+      setCarbGoalValue(dietGoals.carbGoal);
+      setProteinGoalValue(dietGoals.proteinGoal);
+      setFatGoalValue(dietGoals.fatGoal);
+
+      setDatas([
+        {
+          title: `${Math.round(dietGoals.carbGoal)} g`,
+          img: require("../../../assets/images/carbs.png"),
+          text: "Carbs:",
+        },
+        {
+          title: `${Math.round(dietGoals.proteinGoal)} g`,
+          img: require("../../../assets/images/protein.png"),
+          text: "Protein:",
+        },
+        {
+          title: `${Math.round(dietGoals.fatGoal)} g`,
+          img: require("../../../assets/images/fats.png"),
+          text: "Fats:",
+        },
+      ]);
+      setIsLoading(false);
+    };
+
+    getDataOnLoad();
+  }, []);
 
   return (
     <RNModal
@@ -21,7 +125,70 @@ export default function EditMacroGoalsModal({ isVisible, setVisible }) {
       style={styles.modal}
     >
     <View style={styles.container}>
-        <Text>test</Text>
+      <Header showCenter={false} />
+      <Spinner visible={isLoading}></Spinner>
+      <View style={styles.contentContainerStyle}>
+        <Text style={styles.title}>Goals</Text>
+        <View style={styles.macroContainerStyle}>
+          <View style={[styles.item, styles.head]}>
+            <View style={[styles.item, styles.headItem]}>
+              <View style={styles.row}>
+                <Image
+                  source={require("../../../assets/images/calories.png")}
+                  style={styles.calorieItemImg}
+                />
+                <Text style={styles.calorieText}>Calories</Text>
+              </View>
+              <TouchableOpacity
+                onPress={() => {
+                  updateMacrosRef.current.open({
+                    title: "Calories",
+                    isCal: true,
+                    units: calorieUnit,
+                    value: calories,
+                  });
+                }}
+              >
+                <Image
+                  style={styles.calorieEditImg}
+                  source={require("../../../assets/images/edit.png")}
+                />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.calorieTitle}>
+              {calories} {calorieUnit}
+            </Text>
+          </View>
+          {datas.map((item, index) => (
+            <View style={styles.item} key={index}>
+              <View style={styles.row}>
+                <Image source={item.img} style={styles.itemImg} />
+                <Text style={styles.text}>{item.text}</Text>
+                <Text style={styles.itemTitle}>{item.title}</Text>
+              </View>
+              <TouchableOpacity
+                onPress={() => {
+                  updateMacrosRef.current.open({
+                    title: item.text,
+                    isCal: false,
+                    units: "g",
+                    value: item.title,
+                  });
+                }}
+              >
+                <Image
+                  style={styles.editImg}
+                  source={require("../../../assets/images/edit.png")}
+                />
+              </TouchableOpacity>
+            </View>
+          ))}
+        </View>
+      </View>
+      <UpdateMacrosModal
+        getRef={(ref) => (updateMacrosRef.current = ref)}
+        onUpdateMacroValue={onUpdateMacroValue}
+      />
     </View>
       
     </RNModal>
@@ -29,60 +196,97 @@ export default function EditMacroGoalsModal({ isVisible, setVisible }) {
 }
 
 const styles = StyleSheet.create({
-  modal: {
-    margin: 0,
-    alignItems: "center",
-    justifyContent: "center",
-  },
   container: {
-    width: "90%",
-    paddingVertical: 15,
-    backgroundColor: "white",
-    borderRadius: 30,
-    borderWidth: 1,
-    borderBlockColor: "rgba(0,0,0,0.5)",
+    flex: 1,
+    backgroundColor: "#fff",
   },
-  line: {
-    borderBottomColor: "#ccc",
-    borderBottomWidth: 2,
+  contentContainerStyle: {
+    paddingTop: 20,
   },
-  titleText: {
-    fontFamily: "Sego",
-    fontSize: 33,
+  macroContainerStyle: {
+    paddingTop: 50,
+  },
+  title: {
+    fontFamily: "Sego-Bold",
+    fontSize: 36,
     color: "#25436B",
-    marginVertical: 20,
+    marginVertical: 25,
     alignSelf: "center",
   },
-  rowText: {
-    fontSize: 24,
+  head: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    paddingHorizontal: 20,
+  },
+  headItem: {
+    borderWidth: 0,
+    padding: 0,
+    width: "100%",
+    marginHorizontal: 0,
+    paddingVertical: 0,
+  },
+  item: {
+    borderWidth: 1,
+    borderColor: "#CCCCCC",
+    borderRadius: 30,
+    marginHorizontal: 20,
+    marginBottom: 15,
+    padding: 10,
+    paddingVertical: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  itemTitle: {
+    fontSize: 16,
     fontFamily: "Sego-Bold",
     color: "#25436B",
+    marginLeft: 15,
+  },
+  calorieTitle: {
+    fontFamily: "Sego-Bold",
+    color: "#25436B",
+    fontSize: 22,
+  },
+  calorieText: {
+    fontSize: 20,
+    fontFamily: "Sego",
+    color: "#25436B",
+  },
+  calorieEditImg: {
+    width: 30,
+    height: 30,
+  },
+  calorieItemImg: {
+    width: 30,
+    height: 30,
+    marginRight: 5,
+  },
+  itemImg: {
+    width: 24,
+    height: 24,
+    marginRight: 5,
   },
   row: {
     flexDirection: "row",
     alignItems: "center",
-    paddingVertical: 7,
   },
-  icon: {
-    height: 30,
-    width: 30,
-    marginRight: 10,
-    marginLeft: 50,
+  editImg: {
+    width: 24,
+    height: 24,
   },
-  button: {
-    borderRadius: 25,
-    borderWidth: 2,
-    alignItems: "center",
-    width: "60%",
-    padding: 5,
-    alignSelf: "center",
-    alignContent: "center",
-    marginTop: 30,
-    marginBottom: 15,
-  },
-  buttonText: {
-    fontSize: 24,
+  text: {
+    fontSize: 16,
     fontFamily: "Sego",
     color: "#25436B",
+  },
+  flex: {
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  minitext: {
+    fontSize: 16,
+    marginTop: 10,
   },
 });
