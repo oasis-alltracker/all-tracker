329f59e05cb9aa3551e9ae923cd455a611fa15a5
basmaabdulrazaq <abdulrab@myumanitoba.ca>
2025-06-06 14:28:29 -0500

    working api

--- backend/tracker-diet/recentEntries/getRecentEntries.js
+++ backend/tracker-diet/recentEntries/getRecentEntries.js
@@ -5,12 +5,11 @@ class GetRecentEntries {
 
   async getRecentEntries(user) {
     try {
-      await this.get(user);
-      foodEntries = "hi";
+      const foodEntries = await this.get(user.email);
 
       return {
         statusCode: 200,
-        body: JSON.stringify(sums),
+        body: JSON.stringify(foodEntries),
         headers: {
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Credentials": true,
@@ -29,18 +28,20 @@ class GetRecentEntries {
     }
   }
 
-  findUniqueEntries(entries){
-    var result = {}
-    var count =0;
-    entries.forEach(item => {
-      const key = item.name;
-      if(!result[key] && count<10)
-      {
-        result[key] = item;
-        count ++;
+  findUniqueEntries(entries) {
+    var resultNames = {};
+    var result = [];
+    var count = 0;
+    var i;
+    for (i = 0; i < entries.length && count < 10; i++) {
+      const key = entries[i].name;
+      if (!resultNames[key]) {
+        resultNames[key] = 1;
+        result.push(entries[i]);
+        count++;
       }
-    });
-
+    }
+    return result;
   }
 
   async get(user) {
@@ -51,10 +52,10 @@ class GetRecentEntries {
     const values = {
       ":pk": `${user}-foodEntry`,
     };
+
     const response = await this.DB.queryItem(expression, names, values, 30);
     var foodItems = this.findUniqueEntries(response?.Items);
-
-    return sums;
+    return foodItems;
   }
 }
 module.exports = GetRecentEntries;
