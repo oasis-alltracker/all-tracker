cb4bc9cf98b3b0b87765069b11d5729daf2a9df9
Karin Plavin <98622976+KarinP03@users.noreply.github.com>
2025-06-23 16:12:54 -0500

    Barcode camera page improvements (#125)
    * Removed flash
    
    * Added scan delay
    
    * Added spinner and adjusted timeout length
    
    * Changed screen layout to just have white back button
    
    * Added instruction toast
    
    * Added and formatted barcode viewfinder
    
    * Remoevd permissions alert, only device specific permissions request triggers
    
    * Updated back button
    
    * Adjusted viewfinder size and format
    
    * Added setScanned(false) to useFocusEffect for safety
    
    * Moved permissions to a useEffect
    
    * Improved barcode types specifying to resolve typo

--- mobile-app/src/screens/Fitness-Diet/mealPages/BarcodeCamera.js
+++ mobile-app/src/screens/Fitness-Diet/mealPages/BarcodeCamera.js
@@ -1,58 +1,43 @@
 import { CameraView, useCameraPermissions } from "expo-camera";
-import { useState } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { StyleSheet, TouchableOpacity, View, Image, Alert } from "react-native";
 import Toast from "react-native-root-toast";
+import Spinner from "react-native-loading-spinner-overlay";
 import navigationService from "../../../navigators/navigationService";
+import { useFocusEffect } from "@react-navigation/native";
 
 const BarcodeCamera = ({ route }) => {
   const [permission, requestPermission] = useCameraPermissions();
-  const [flash, setFlash] = useState("off");
   const [scanned, setScanned] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  if (!permission) {
-    // Camera permissions are still loading.
-    return <View />;
-  }
-
-  if (!permission.granted) {
-    //Camera permissions are not granted yet.
-    Alert.alert(
-      "Camera Permission Needed",
-      "Barcode scanning requires camera access.",
-      [
-        { text: "Cancel", style: "cancel" },
-        {
-          text: "Grant Permission",
-          isPreferred: true,
-          onPress: () => {
-            requestPermission();
-          },
-        },
-      ],
-      {
-        cancelable: true,
-      }
-    );
-  }
-
-  const toggleFlash = () => {
-    if (flash === "off") {
-      setFlash("on");
-    } else {
-      setFlash("off");
+  useFocusEffect(
+    useCallback(() => {
+      setScanned(false);
+      Toast.show("Please place food barcode\nin view of the camera.", {
+        ...styles.errorToast,
+        duration: Toast.durations.SHORT,
+        position: Toast.positions.CENTER,
+      });
+    }, [])
+  );
+  useEffect(() => {
+    if (permission && !permission.granted) {
+      setIsLoading(true);
+      requestPermission();
+      setIsLoading(false);
     }
-    Toast.show("Flash is " + flash, {
-      ...styles.errorToast,
-      duration: Toast.durations.SHORT,
-      position: Toast.positions.CENTER,
-    });
-  };
+  }, [permission]);
 
   const handleScannedResult = (barcodeScanningResult) => {
     if (!scanned) {
       setScanned(true);
-      exitPage(barcodeScanningResult);
-      setTimeout(() => setScanned(false), 3000);
+      setIsLoading(true);
+      setTimeout(() => {
+        setScanned(false);
+        setIsLoading(false);
+        exitPage(barcodeScanningResult);
+      }, 1500);
     }
   };
 
@@ -74,36 +59,27 @@ const BarcodeCamera = ({ route }) => {
 
   return (
     <View style={styles.container}>
-      <View style={[styles.banner, styles.topArea]}>
+      <Spinner visible={isLoading}></Spinner>
+      <CameraView
+        style={styles.camera}
+        barcodeScannerSettings={{
+          barcodeTypes: ["ean13", "ean8", "upc_e", "upc_a"],
+        }}
+        onBarcodeScanned={handleScannedResult}
+      >
         <TouchableOpacity onPress={() => exitPage(null)}>
           <Image
             style={styles.backArrow}
             source={require("../../../assets/images/back-arrow.png")}
           ></Image>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => toggleFlash()}>
+        <View style={styles.viewfinderContainer}>
           <Image
-            style={styles.flashIcon}
-            source={require("../../../assets/images/flash.png")}
+            style={styles.viewfinder}
+            source={require("../../../assets/images/barcode-viewfinder.png")}
           ></Image>
-        </TouchableOpacity>
-      </View>
-      <CameraView
-        style={styles.camera}
-        flash={flash}
-        barcodeScannerSettings={{
-          barcodetypes: ["ean13", "ean8", "upc_e", "upc_a"],
-        }}
-        onBarcodeScanned={handleScannedResult}
-      ></CameraView>
-      <View style={styles.banner}>
-        <TouchableOpacity>
-          <Image
-            style={styles.scanIcon}
-            source={require("../../../assets/images/barcode.png")}
-          ></Image>
-        </TouchableOpacity>
-      </View>
+        </View>
+      </CameraView>
     </View>
   );
 };
@@ -113,37 +89,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
   },
-  banner: {
-    backgroundColor: "#D7F6FF",
-    alignItems: "center",
-  },
-  topArea: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
   backArrow: {
     height: 50,
     width: 50,
     marginTop: 60,
-    marginBottom: 20,
     marginLeft: 20,
+    tintColor: "white",
   },
-  flashIcon: {
-    height: 50,
-    width: 50,
-    marginTop: 60,
-    marginBottom: 20,
-    marginRight: 20,
+  viewfinderContainer: {
+    flex: 1,
+    alignItems: "center",
   },
-  scanIcon: {
-    height: 80,
-    width: 80,
-    marginBottom: 50,
-    marginTop: 10,
-  },
-  message: {
-    textAlign: "center",
-    paddingBottom: 10,
+  viewfinder: {
+    height: "37.5%",
+    width: "80%",
+    marginTop: 190,
+    tintColor: "white",
   },
   camera: {
     flex: 1,
