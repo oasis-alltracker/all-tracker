0700ca03c353b8ece361e9280b9151f4cf9e3cef
basmaabdulrazaq <abdulrab@myumanitoba.ca>
2025-06-24 11:04:20 -0500

    adjusted backend calculation of meal macros

--- backend/tracker-diet/foodEntriesMacros/getMealMacros.js
+++ backend/tracker-diet/foodEntriesMacros/getMealMacros.js
@@ -1,107 +1,114 @@
 class GetMealMacros {
-    constructor(db) {
-      this.DB = db;
-    }
+  constructor(db) {
+    this.DB = db;
+  }
 
-    calcSums(response)
-    {
-      var result = {};
+  calcSums(response) {
+    var result = {};
 
-      response.forEach(item => {
-          const key = item.meal;
+    response.forEach((item) => {
+      const key = item.meal;
 
-          if(!result[key]){
-            result[key] = {
-              calorieCount: 0,
-              fatCount: 0,
-              proteinCount: 0,
-              carbCount: 0,
-              entries: [],
-            }
-          }
-          result[key]["calorieCount"] += item.calorieCount;
-          result[key]["fatCount"] += item.fatCount;
-          result[key]["carbCount"] += item.carbCount;
-          result[key]["proteinCount"] += item.proteinCount;
-          result[key]["entries"].push(item);
-      });
-
-      return result;
-    }
-
-    async getMealMacros(user, queryStringParameters) {
-      try {
-        var sums;
-        if (queryStringParameters.dateStamp && queryStringParameters.meal) {
-          sums = await this.getSumsForOneMeal(
-            user.email,
-            queryStringParameters.dateStamp,
-            queryStringParameters.meal,
-          );
-        } else if (
-            queryStringParameters.dateStamp 
-        ) {
-          sums = await this.getSumsForOneDay(
-            user.email,
-            queryStringParameters.dateStamp,
-          );
-        } else{
-            throw new Error("Missing queryStringParameters");
-        }
-
-        return {
-          statusCode: 200,
-          body: JSON.stringify(sums),
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true,
-          },
-        };
-      } catch (e) {
-        console.log(e);
-        return {
-          statusCode: 500,
-          body: JSON.stringify("Request failed"),
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true,
-          },
+      if (!result[key]) {
+        result[key] = {
+          calorieCount: 0,
+          fatCount: 0,
+          proteinCount: 0,
+          carbCount: 0,
+          entries: [],
         };
       }
+
+      //adding it as an integer (as it will have 2 decimal points at most)
+      result[key]["calorieCount"] += item.calorieCount * 100;
+      result[key]["fatCount"] += item.fatCount * 100;
+      result[key]["carbCount"] += item.carbCount * 100;
+      result[key]["proteinCount"] += item.proteinCount * 100;
+      result[key]["entries"].push(item);
+    });
+
+    //converting final result back to decimal format
+    for (const key in result) {
+      result[key]["calorieCount"] = result[key]["calorieCount"] / 100;
+      result[key]["fatCount"] = result[key]["fatCount"] / 100;
+      result[key]["carbCount"] = result[key]["carbCount"] / 100;
+      result[key]["proteinCount"] = result[key]["proteinCount"] / 100;
     }
 
-    async getSumsForOneDay(user, dateStamp) {
-      const expression = "#pk = :pk AND begins_with(#sk, :sk) ";
-      const names = {
-        "#pk": "PK",
-        "#sk": "SK",
+    return result;
+  }
+
+  async getMealMacros(user, queryStringParameters) {
+    try {
+      var sums;
+      if (queryStringParameters.dateStamp && queryStringParameters.meal) {
+        sums = await this.getSumsForOneMeal(
+          user.email,
+          queryStringParameters.dateStamp,
+          queryStringParameters.meal
+        );
+      } else if (queryStringParameters.dateStamp) {
+        sums = await this.getSumsForOneDay(
+          user.email,
+          queryStringParameters.dateStamp
+        );
+      } else {
+        throw new Error("Missing queryStringParameters");
+      }
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(sums),
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
       };
-      const values = {
-        ":pk": `${user}-foodEntry`,
-        ":sk": dateStamp,
+    } catch (e) {
+      console.log(e);
+      return {
+        statusCode: 500,
+        body: JSON.stringify("Request failed"),
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
       };
-
-      const response = await this.DB.queryItem(expression, names, values);
-      var sums = this.calcSums(response?.Items);
-
-      return sums;
-    }
-
-    async getSumsForOneMeal(user, dateStamp, meal) {
-      const expression = "#pk = :pk AND begins_with(#sk, :sk)";
-      const names = {
-        "#pk": "PK",
-        "#sk": "SK",
-      };
-      const values = {
-        ":pk": `${user}-foodEntry`,
-        ":sk": `${dateStamp}-${meal}`,
-      };
-
-      const response = await this.DB.queryItem(expression, names, values);
-      const sums = this.calcSums(response?.Items);
-
-      return sums;
     }
   }
-  module.exports = GetMealMacros;
+
+  async getSumsForOneDay(user, dateStamp) {
+    const expression = "#pk = :pk AND begins_with(#sk, :sk) ";
+    const names = {
+      "#pk": "PK",
+      "#sk": "SK",
+    };
+    const values = {
+      ":pk": `${user}-foodEntry`,
+      ":sk": dateStamp,
+    };
+
+    const response = await this.DB.queryItem(expression, names, values);
+    var sums = this.calcSums(response?.Items);
+
+    return sums;
+  }
+
+  async getSumsForOneMeal(user, dateStamp, meal) {
+    const expression = "#pk = :pk AND begins_with(#sk, :sk)";
+    const names = {
+      "#pk": "PK",
+      "#sk": "SK",
+    };
+    const values = {
+      ":pk": `${user}-foodEntry`,
+      ":sk": `${dateStamp}-${meal}`,
+    };
+
+    const response = await this.DB.queryItem(expression, names, values);
+    const sums = this.calcSums(response?.Items);
+
+    return sums;
+  }
+}
+module.exports = GetMealMacros;
