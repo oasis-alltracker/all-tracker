6cf4f50bce3d9c7defb0fdccdbdefa46f2488f5d
KarinP03 <k.plavin03@gmail.com>
2025-07-30 12:16:09 -0500

    Updated toast positioning in calendar component

--- mobile-app/src/components/Calendar.js
+++ mobile-app/src/components/Calendar.js
@@ -515,7 +515,7 @@ const DatePicker = ({ getRef, saveDateHandler }) => {
           </TouchableOpacity>
         </View>
       </View>
-      <Toast position="top" />
+      <Toast position="top" topOffset={60} visibilityTime={2500} />
     </Modal>
   );
 };
