dc27be847eb9ef01c5a07e2805e588f16e3fc20d
Aidan Andrew-Hodgert <hodgerta@gmail.com>
2025-08-12 08:06:09 -0600

    Track update stats on page index updates

--- mobile-app/src/screens/Fitness-Diet/index.js
+++ mobile-app/src/screens/Fitness-Diet/index.js
@@ -415,9 +415,8 @@ const FitnessDiet = ({ navigation, route }) => {
                 style={[
                   sharedStyles.dot,
                   key === pageIndex && {
-                    backgroundColor:
-                      ValueSheet.colours[theme][theme].primaryColour,
-                    borderColor: ValueSheet.colours[theme][theme].borderNavy,
+                    backgroundColor: ValueSheet.colours[theme].primaryColour,
+                    borderColor: ValueSheet.colours[theme].borderNavy,
                   },
                 ]}
               />
