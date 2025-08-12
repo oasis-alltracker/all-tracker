0650adea9b5d7da7bf038852a5097fae1dde28a3
basmaabdulrazaq <abdulrab@myumanitoba.ca>
2025-08-12 14:15:45 -0500

    manually storing theme when initalizing

--- mobile-app/src/contexts/ThemeProvider.js
+++ mobile-app/src/contexts/ThemeProvider.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState, useEffect } from "react";
 import { Appearance } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const ThemeContext = createContext();
 
@@ -9,9 +10,35 @@ export const ThemeProvider = ({ children }) => {
   );
 
   useEffect(() => {
-    const listener = Appearance.addChangeListener(({ colorScheme }) => {
+    var skip = false;
+
+    const initializeTheme = async () => {
+      skip = true;
+      const colorScheme = await AsyncStorage.getItem("theme");
+      if (colorScheme && (colorScheme == "light" || colorScheme == "dark")) {
+        Appearance.setColorScheme(theme);
+      } else {
+        Appearance.setColorScheme(null);
+      }
+      console.log(
+        "in initialuze theme, color scheme returned from async storage is" +
+          colorScheme
+      );
       setTheme(colorScheme == "dark" ? "darkColours" : "colours");
+      skip = false;
+    };
+
+    initializeTheme();
+
+    const listener = Appearance.addChangeListener(({ colorScheme }) => {
+      console.log("in apperance listener, color scheme is " + colorScheme);
+      if (!skip) {
+        console.log("didnt skip!");
+        setTheme(colorScheme == "dark" ? "darkColours" : "colours");
+        AsyncStorage.setItem("theme", colorScheme);
+      }
     });
+
     return () => listener.remove();
   }, []);
 
