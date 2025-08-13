2e91dd183029ba202b8657760fd7cebeb80f780d
basmaabdulrazaq <abdulrab@myumanitoba.ca>
2025-08-13 11:25:13 -0500

    implemented dark mode using only context

---
+++ mobile-app/src/contexts/ThemeProvider.js
@@ -0,0 +1,39 @@
+import React, { createContext, useState, useEffect } from "react";
+import { Appearance } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  //theme can contain 2 values: light or dark
+  const [theme, setTheme] = useState(Appearance.getColorScheme() || "light");
+
+  useEffect(() => {
+    const initializeTheme = async () => {
+      const colorScheme = await AsyncStorage.getItem("theme");
+      if (colorScheme && (colorScheme == "light" || colorScheme == "dark")) {
+        setTheme(colorScheme);
+      }
+      console.log(
+        "in initialuze theme, color scheme returned from async storage is " +
+          colorScheme
+      );
+    };
+
+    initializeTheme();
+  }, []);
+
+  const updateTheme = async (colorScheme) => {
+    console.log("in update theme, " + colorScheme);
+    if (colorScheme == "dark" || colorScheme == "light") {
+      setTheme(colorScheme);
+      await AsyncStorage.setItem("theme", colorScheme);
+    }
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme: theme, setter: updateTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
