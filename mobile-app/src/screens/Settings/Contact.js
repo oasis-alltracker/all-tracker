6d5ec3da98ffa02b21d60a726ae510f6f739392b
basmaabdulrazaq <abdulrab@myumanitoba.ca>
2025-08-20 15:29:58 -0500

    added dark mode to remaining settings pages

--- mobile-app/src/screens/Settings/Contact.js
+++ mobile-app/src/screens/Settings/Contact.js
@@ -1,27 +1,41 @@
 import { View, Text, StyleSheet } from "react-native";
 import { Header } from "../../components";
 import { ValueSheet } from "../../ValueSheet";
+import { useContext } from "react";
+import { ThemeContext } from "../../contexts/ThemeProvider";
+import { sharedStyles } from "../styles";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Contact = () => {
+  const theme = useContext(ThemeContext).value;
   return (
-    <>
+    <SafeAreaView
+      style={[styles.container, sharedStyles["pageBackground_" + theme]]}
+    >
       <View style={styles.container}>
         <Header showCenter={false} />
-        <Text style={styles.title}>Contact</Text>
+        <Text style={[styles.title, sharedStyles["textColour_" + theme]]}>
+          Contact
+        </Text>
       </View>
       <View style={styles.container}>
-        <Text style={styles.text}>If you would like to contact us,</Text>
-        <Text style={styles.text}>please DM on instagram</Text>
-        <Text style={styles.textBold}>@oasis.journal.app</Text>
+        <Text style={[styles.text, sharedStyles["textColour_" + theme]]}>
+          If you would like to contact us,
+        </Text>
+        <Text style={[styles.text, sharedStyles["textColour_" + theme]]}>
+          please DM on instagram
+        </Text>
+        <Text style={[styles.textBold, sharedStyles["textColour_" + theme]]}>
+          @oasis.journal.app
+        </Text>
       </View>
-    </>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: ValueSheet.colours.background,
   },
   title: {
     alignSelf: "center",
